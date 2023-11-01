91a26b5905a026474a255c35dbac6800dca62b26
MurtaghLuke <D00246562@student.dkit.ie>
2023-11-01 19:05:11 +0000

    Created variables, constructor, getters, setters and toString method in Activity class

--- src/Activity.java
+++ src/Activity.java
@@ -1,3 +1,68 @@
 public class Activity
 {
+    private String activityType;
+    private String date;
+    private int duration;
+    private double distance;
+    int averageHeartRate;
+
+
+    public Activity(String activityType, String date, int duration, double distance, int averageHeartRate) {
+        this.activityType = activityType;
+        this.date = date;
+        this.duration = duration;
+        this.distance = distance;
+        this.averageHeartRate = averageHeartRate;
+    }
+
+    public String getActivityType() {
+        return activityType;
+    }
+
+    public void setActivityType(String activityType) {
+        this.activityType = activityType;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public int getAverageHeartRate() {
+        return averageHeartRate;
+    }
+
+    public void setAverageHeartRate(int averageHeartRate) {
+        this.averageHeartRate = averageHeartRate;
+    }
+
+    @Override
+    public String toString() {
+        return "Activity{" +
+                "activityType='" + activityType + '\'' +
+                ", date='" + date + '\'' +
+                ", duration=" + duration +
+                ", distance=" + distance +
+                ", averageHeartRate=" + averageHeartRate +
+                '}';
+    }
 }
