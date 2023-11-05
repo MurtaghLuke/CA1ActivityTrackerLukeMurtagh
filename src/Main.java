1012d56d0221c2d8abfad4609cd82db744966d19
MurtaghLuke <D00246562@student.dkit.ie>
2023-11-05 21:58:18 +0000

    User can now view average distance and average calories burned.

--- src/Main.java
+++ src/Main.java
@@ -42,15 +42,17 @@ public class Main {
 
     public static void displayMenu(){
         System.out.println("0: Exit");
-        System.out.println("1: Sort by calories burned (Descending)");
+        System.out.println("1: Sort by type of activity");
         System.out.println("2: Sort by date (Ascending)");
         System.out.println("3: Sort by date (Descending)");
         System.out.println("4: Sort by activity duration (Ascending)");
         System.out.println("5: Sort by activity duration (Descending)");
-        System.out.println("6: Sort by type of activity");
+        System.out.println("6: Sort by calories burned (Descending)");
         System.out.println("7: Sort by distance (Ascending)");
-        System.out.printf("8: Sort by distance (Descending)\n");
-        System.out.printf("\n9: view a subset of activities based on specific fields\n");
+        System.out.println("8: Sort by distance (Descending)");
+        System.out.println("9: view a subset of activities based on specific fields");
+        System.out.println("10: View average distance per activity");
+        System.out.println("11: View average calories burned");
     }
 
 
@@ -71,6 +73,24 @@ public class Main {
 
 
 
+    public static double calculateAverageDistance(ArrayList<Activity> activities) {
+        double totalDistance = 0.0;
+        for (Activity activity : activities) {
+            totalDistance += activity.getDistance();
+        }
+        return totalDistance / activities.size();
+    }
+
+    public static double calculateAverageCaloriesBurned(ArrayList<Activity> activities) {
+        double totalCaloriesBurned = 0.0;
+        for (Activity activity : activities) {
+            totalCaloriesBurned += activity.getCaloriesBurned();
+        }
+        return totalCaloriesBurned / activities.size();
+    }
+
+
+
 
     public static void main(String[] args) throws IOException {
         ArrayList<Activity> activities = new ArrayList<>();
@@ -87,43 +107,47 @@ public class Main {
 
             switch (choice) {
                 case 1:
-                    Collections.sort(activities, new CaloriesBurnedComparator());
+                    Collections.sort(activities, new ActivityNameComparator());
                     displayTable(activities);
+                    System.out.println();
                     break;
                 case 2:
                     Collections.sort(activities, new DateComparator());
                     displayTable(activities);
+                    System.out.println();
                     break;
                 case 3:
                     Collections.sort(activities, Collections.reverseOrder(new DateComparator()));
                     displayTable(activities);
+                    System.out.println();
                     break;
                 case 4:
                     Collections.sort(activities, new DurationComparator());
                     displayTable(activities);
+                    System.out.println();
                     break;
                 case 5:
                     Collections.sort(activities, Collections.reverseOrder(new DurationComparator()));
                     displayTable(activities);
+                    System.out.println();
                     break;
                 case 6:
-                    Collections.sort(activities, new ActivityNameComparator());
+                    Collections.sort(activities, new CaloriesBurnedComparator());
                     displayTable(activities);
+                    System.out.println();
                     break;
                 case 7:
                     //lambda
                     activities.sort((s1, s2) -> Double.compare(s1.getDistance(), s2.getDistance()));
                     displayTable(activities);
+                    System.out.println();
                     break;
                 case 8:
                     //lambda
                     activities.sort((s1, s2) -> Double.compare(s2.getDistance(), s1.getDistance())); // Reverse order
                     System.out.println(activities);
+                    System.out.println();
                     break;
-
-
-
-
                 case 9:   //Allow the user view a subset of their activity based on specific fields
                     input.nextLine();
                     System.out.print("Enter the activity type (Running, Swimming, Cycling): ");
@@ -135,7 +159,6 @@ public class Main {
                     String userEnergyExpendedType = input.nextLine();
                     System.out.print("Enter the minimum duration as a whole number: ");
                     int userMinDuration = input.nextInt();
-
 
                     // filtered activities arraylist
                     ArrayList<Activity> filteredActivities = new ArrayList<>();
@@ -150,6 +173,17 @@ public class Main {
                     }
 
                     displayTable(filteredActivities);
+                    System.out.println();
+                    break;
+                case 10:
+                    double averageDistance = calculateAverageDistance(activities);
+                    System.out.println("Average distance per activity: " + averageDistance);
+                    System.out.println();
+                    break;
+                case 11:
+                    double averageCaloriesBurned = calculateAverageCaloriesBurned(activities);
+                    System.out.println("Average calories burned: " + averageCaloriesBurned);
+                    System.out.println();
                     break;
             }
         }
