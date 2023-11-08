dee3ce12c31ac45ebdbf210c5d7e778a73ae30e2
MurtaghLuke <l.murtagh2m@gmail.com>
2023-11-08 12:41:06 +0000

    Date comparator can now sort the dates by day, month and year.

--- src/DateComparator.java
+++ src/DateComparator.java
@@ -3,12 +3,31 @@ import java.util.Comparator;
 public class DateComparator implements Comparator<Activity> {
 
     @Override
-    public int compare(Activity activity1, Activity activity2) {
-        String dateStr1 = activity1.getDate();
-        String dateStr2 = activity2.getDate();
+    public int compare(Activity date1, Activity date2) {
+        int[] dateInt1 = parseDate(date1.getDate());
+        int[] dateInt2 = parseDate(date2.getDate());
 
-        // Assuming dateStr1 and dateStr2 are in the format "dd/MM/yyyy"
-        return dateStr1.compareTo(dateStr2);
+        if(dateInt1[2]== dateInt2[2])
+        {
+            if(dateInt1[1]== dateInt2[1])
+            {
+                return dateInt1[0]- dateInt2[0];
+            }
+            return dateInt1[1]- dateInt2[1];
+        }
+        return dateInt1[2]- dateInt2[2];
+    }
+
+    public int[] parseDate(String s)
+    {
+        s = s.trim();
+        String[] dateStr = s.split("/");
+        int[] dates = new int[dateStr.length];
+        for(int i = 0; i < dateStr.length;i++)
+        {
+            dates[i] = Integer.parseInt(dateStr[i]);
+        }
+        return dates;
     }
 
 
